246b3d9ce9e83f579c25968df9b0cdfa49cd5197
prashantpkg777-blip <prashantpkg777@gmail.com>
2025-10-03 21:35:07 +0530

    NEW

---
+++ LOVE.C
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    // I
+    printf("      ***********      \n");
+    printf("         *****         \n");
+    printf("         *****         \n");
+    printf("         *****         \n");
+    printf("         *****         \n");
+    printf("      ***********      \n\n");
+
+    // HEART
+    printf("    ***       ***      \n");
+    printf(" *******   *******     \n");
+    printf("*******************    \n");
+    printf("*******************    \n");
+    printf(" *****************     \n");
+    printf("   *************       \n");
+    printf("     *********         \n");
+    printf("       *****           \n");
+    printf("        ***            \n");
+    printf("         *             \n\n");
+
+    // Y
+    printf(" **           **       \n");
+    printf("  **         **        \n");
+    printf("   **       **         \n");
+    printf("    **     **          \n");
+    printf("     **   **           \n");
+    printf("      ** **            \n");
+    printf("       ***             \n");
+    printf("       ***             \n");
+    printf("       ***             \n\n");
+
+    // O
+    printf("     ***********       \n");
+    printf("   **           **     \n");
+    printf("  **             **    \n");
+    printf("  **             **    \n");
+    printf("   **           **     \n");
+    printf("     ***********       \n\n");
+
+    // U
+    printf(" **             **     \n");
+    printf(" **             **     \n");
+    printf(" **             **     \n");
+    printf(" **             **     \n");
+    printf("  **           **      \n");
+    printf("    ***********        \n");
+
+    return 0;
+}
